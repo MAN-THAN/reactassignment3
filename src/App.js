ca1b64e5f0ec8d3fe240f198ab372be348d44dd7
Manthan123456 <manthantyagi04@gmail.com>
2022-02-25 19:11:54 +0530

    assignment3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Box from './Box';
+import Feedback from "./Feedback"
+import {BrowserRouter as Router, Routes, Route, Navigate, Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    
+  render() {
+    return (
+      <Box />
+      
+      
+
+      
+    )
+  }
 }
 
-export default App;
+export default App
